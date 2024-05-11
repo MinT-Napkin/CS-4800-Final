1fe888ab58b0c613464b13d0bff0a7f076d9a30c
Marvin S <mpsevilla@cpp.edu>
2024-05-11 15:32:41 -0700

    Added missing attributes
    Was missing attributes for:
    Restaurant of order
    Customer placing the order
    Driver delivering order
    Order times for creation, driver pickup, and delivery

--- src/main/java/meal/Order.java
+++ src/main/java/meal/Order.java
@@ -1,9 +1,26 @@
 package meal;
 
+import service.Customer;
+import service.Driver;
+import service.Restaurant;
+
 import java.util.ArrayList;
 import java.util.List;
 
 public class Order {
+    private Restaurant restaurantOfOrigin;
+    private Customer recievingCustomer; // incl. customer name & diet restriction
+    private Driver driver;
+
+    /*
+    * something about customer w/ this, not sure if
+    * this attribute is correct with regards to what the
+    * proj. instructions are  asking
+     * */
+    private int orderCreationTimeInHrs;
+    private int orderPickedUpTimeInHrs;
+    private int orderDeliveredTimeInHrs;
+
     private List<FoodItem> items = new ArrayList<>();
 
     public void addItem(FoodItem item) {
