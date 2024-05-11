83e867d9653ec23f772a8df09d73fc5f15cc0e56
Marvin S <mpsevilla@cpp.edu>
2024-05-11 15:44:34 -0700

    Undoing changes made today, moving to diff branch

--- src/main/java/meal/Order.java
+++ src/main/java/meal/Order.java
@@ -1,28 +1,9 @@
 package meal;
 
-import service.Customer;
-import service.Driver;
-import service.Restaurant;
-
 import java.util.ArrayList;
 import java.util.List;
 
 public class Order {
-    private Restaurant restaurantOfOrigin;
-    private Customer recievingCustomer; // incl. customer name & diet restriction
-    private Driver driver;
-    
-    private int orderCreationTimeInHrs; // something abt customer w/ this, not sure if implementaiton is correct
-    private int orderPickedUpTimeInHrs;
-    private int orderDeliveredTimeInHrs;
-
-    public Order(Restaurant fromRestaurant, Customer fromCusomter, int givenOrderCreationTime_Hrs)
-    {
-        this.restaurantOfOrigin = fromRestaurant;
-        this.recievingCustomer = fromCusomter;
-        this.orderCreationTimeInHrs = givenOrderCreationTime_Hrs;
-    }
-
     private List<FoodItem> items = new ArrayList<>();
 
     public void addItem(FoodItem item) {
